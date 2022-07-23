5007d39c8bfd20d6c0958ebd67b0485d067b145b
Jesus Islas <jesusislas96@gmail.com>
2022-07-23 10:01:33 -0400

    further file re-structuring

--- server.js
+++ server.js
@@ -9,28 +9,30 @@ const dbNotes = require("./db/db");
 
 const PORT = process.env.PORT || 3001;
 
+app.use(express.static("public"));
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
 // Route to index.html landing page
 app.get("/", (request, response) => {
-    response.sendFile(`${__dirname}/public/index.html`)
+    response.sendFile(path.join(__dirname, 'public', 'index.html'))
 })
 
 // Route to notes.html file 
 app.get("/notes", (request, response) => {
     response.sendFile(`${__dirname}/public/notes.html`)
+    console.log("Hello world")
 })
 
 // Route to read db.json file. 
 app.get("/api/notes", (request, response) => {
-    console.log(dbNotes)
+    console.log(request)
     response.json(dbNotes)
 })
 
 // Route that will receive new note and post it to the front end
 app.post("/api/notes", (request, response) => {
-    
+    console.log(request.body)
 })
 
 // Route that will delete a note
